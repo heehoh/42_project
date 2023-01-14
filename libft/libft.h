f8e2d3e5fd4eab912db2b5f43e3b0c87fd91e996
heeho <114281631+heehoh@users.noreply.github.com>
2023-01-14 20:29:51 +0900

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -27,7 +27,7 @@ void		ft_toupper(char *c);
 void		*ft_memset(void *b, int c, size_t len);
 void		ft_bzero(void *s, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memcpy(const void *src, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t		ft_strlen(const char *s);
