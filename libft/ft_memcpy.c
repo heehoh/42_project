82222dc44ccc0435f25c0ea3ee716e0d4a840e60
heeho <114281631+heehoh@users.noreply.github.com>
2023-01-14 20:29:00 +0900

    Update ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -12,15 +12,17 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(const void *src, size_t n)
 {
 	unsigned char	*ds;
 	unsigned char	*sr;
 	size_t			i;
 
-	if (dst == 0 && src == 0)
-		return (0);
-	ds = (unsigned char *)dst;
+	if (dst == NULL && src == NULL)
+		return (NULL);
+    ds = (unsigned char *)malloc(n);
+    if (ds == NULL)
+        return (NULL);
 	sr = (unsigned char *)src;
 	i = 0;
 	while (i < n)
@@ -28,5 +30,5 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		ds[i] = sr[i];
 		++i;
 	}
-	return (dst);
+	return (ds);
 }
