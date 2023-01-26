31aa46f0a9faa516d24070cecb9d0258bafd51fb
heehoh <hujeong@student.42seoul.kr>
2023-01-26 20:18:41 +0900

    get_path

--- pipex_set_cmd.c
+++ pipex_set_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:19:30 by hujeong           #+#    #+#             */
-/*   Updated: 2023/01/26 20:00:39 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/01/26 20:13:43 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 char	**get_path(char **env)
 {
-	char	*path;
-	char	**paths;
+	char	**path;
 	int		i;
 
 	i = 0;
@@ -25,7 +24,8 @@ char	**get_path(char **env)
 			break ;
 		++i;
 	}
-	return (env[i] + 5);
+	path = ft_split(env[i] + 5, ':');
+	return (path);
 }
 
 void	set_cmd(t_cmd *cmd, char *cmd_options, char **path)
