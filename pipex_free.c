19907ae396c8823ad3612c8795bca233eac1b07c
heehoh <hujeong@student.42seoul.kr>
2023-01-27 20:39:24 +0900

    parent->child->child

--- pipex_free.c
+++ pipex_free.c
@@ -6,11 +6,50 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:57:47 by hujeong           #+#    #+#             */
-/*   Updated: 2023/01/26 19:59:58 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/01/27 20:24:06 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	free_path(char **path);
-void	free_cmd(t_cmd *cmd);
+void	free_path(char **path)
+{
+	int	i;
+
+	i = 0;
+	while (path[i])
+	{
+		free(path[i]);
+		path[i] = NULL;
+		++i;
+	}
+	free(path);
+	path = NULL;
+}
+
+void	free_cmd(t_cmd *cmd)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (++i < 2)
+	{
+		j = -1;
+		while (cmd[i].path[++j])
+		{
+			free(cmd[i].path[j]);
+			cmd[i].path[j] = NULL;
+		}
+		free(cmd[i].path);
+		cmd[i].path = NULL;
+		j = -1;
+		while (cmd[i].option[++j])
+		{
+			free(cmd[i].option[j]);
+			cmd[i].option[j] = NULL;
+		}
+		free(cmd[i].option);
+		cmd[i].option = NULL;
+	}
+}
--- pipex_set_cmd.c
+++ pipex_set_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:19:30 by hujeong           #+#    #+#             */
-/*   Updated: 2023/01/26 20:13:43 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/01/27 20:22:21 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,25 @@ char	**get_path(char **env)
 
 void	set_cmd(t_cmd *cmd, char *cmd_options, char **path)
 {
+	char	*cmd_without_path;
+	int		i;
 
+	cmd->option = ft_split(cmd_options, ' ');
+	if (cmd->option == NULL)
+		perror("ft_split fail");
+	cmd_without_path = ft_strjoin("/", cmd->option[0]);
+	i = 0;
+	while (path[i])
+		++i;
+	cmd->path = (char **)malloc(sizeof(char *) * (i + 1));
+	if (cmd->path == NULL)
+		perror("malloc fail");
+	i = 0;
+	while (path[i])
+	{
+		cmd->path[i] = ft_strjoin(cmd_without_path, path[i]);
+		++i;
+	}
+	cmd->path[i] = NULL;
+	free(cmd_without_path);
 }
-
-void	free_cmd(t_cmd *cmd)
-{}
