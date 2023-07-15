95d35ec5e54075da67aca6447722a339929802e9
hujeong <hujeong@student.42seoul.kr>
2023-07-15 10:01:06 +0900

    refactor

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -5,29 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/15 08:45:13 by hujeong           #+#    #+#             */
-/*   Updated: 2023/07/15 08:56:58 by hujeong          ###   ########.fr       */
+/*   Created: 2023/07/15 09:04:42 by hujeong           #+#    #+#             */
+/*   Updated: 2023/07/15 10:01:06 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-
-int main() {
-  {
-    Weapon club = Weapon("crude spiked club");
-    HumanA bob("Bob", club);
-    bob.attack();
-    club.setType("some other type of club");
-    bob.attack();
-  }
-  {
-    Weapon club = Weapon("crude spiked club");
-    HumanB jim("Jim");
-    jim.setWeapon(club);
-    jim.attack();
-    club.setType("some other type of club");
-    jim.attack();
-  }
-  return 0;
-}
+int main(int ac, char **av) {}
