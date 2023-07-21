04ca27fcaa78bb7588f706e9e7e84d1d7cf6ce66
hujeong <hujeong@student.42seoul.kr>
2023-07-21 13:52:05 +0900

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,11 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: hujeong <hujeong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 09:04:42 by hujeong           #+#    #+#             */
-/*   Updated: 2023/07/15 10:01:06 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/07/18 15:04:30 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av) {}
+#include <fstream>
+
+int main(int ac, char **av) {
+  if (ac != 4) return 1;
+  std::ifstream ifs;
+  ifs.open(av[1]);
+  if (!ifs.is_open()) return 1;
+  std::ofstream ofs;
+  ofs.open(std::string(av[1]) + ".replace");
+  if (!ofs.is_open()) return 1;
+  std::string s1(av[2]);
+  std::string s2(av[3]);
+  std::string line;
+  while (std::getline(ifs, line)) {
+    size_t pos;
+    while ((pos = line.find(s1, pos)) != std::string::npos) {
+      line.erase(pos, s1.size());
+      line.insert(pos, s2);
+    }
+    ofs << line << std::endl;
+  }
+  ifs.close();
+  ofs.close();
+  return 0;
+}
