958926c6494938bffa32d5c7bdf9100ad38df719
hujeong <hujeong@student.42seoul.kr>
2023-09-02 12:10:25 +0900

    ex02

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,97 +6,103 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 13:24:46 by hujeong           #+#    #+#             */
-/*   Updated: 2023/08/26 19:38:30 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/09/02 12:02:37 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
-#include <iostream>
 
 // Orthodox Canonical form
-ClapTrap::ClapTrap(void) : name("무명"), hitPoints(C_HP), energyPoints(C_EP), attackDamage(C_AD){
+ClapTrap::ClapTrap(void)
+    : name("무명"),
+      hitPoints(claptrap::HP),
+      energyPoints(claptrap::EP),
+      attackDamage(claptrap::AD) {
   std::cout << "ClapTrap 기본 생성자 호출" << std::endl;
 }
 
-ClapTrap::ClapTrap(std::string name) : name(name), hitPoints(C_HP), energyPoints(C_EP), attackDamage(C_AD) {
+ClapTrap::ClapTrap(std::string name)
+    : name(name),
+      hitPoints(claptrap::HP),
+      energyPoints(claptrap::EP),
+      attackDamage(claptrap::AD) {
   std::cout << "ClapTrap 생성자 호출" << std::endl;
 }
 
-ClapTrap::ClapTrap(const ClapTrap& src) {
+ClapTrap::ClapTrap(const ClapTrap& src)
+    : name(src.getName()),
+      hitPoints(src.getHitPoints()),
+      energyPoints(src.getEnergyPoints()),
+      attackDamage(src.getAttackDamage()) {
   std::cout << "ClapTrap 복사 생성자 호출" << std::endl;
-  name = src.name;
-  hitPoints = src.hitPoints;
-  energyPoints = src.energyPoints;
-  attackDamage = src.attackDamage;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& src) {
   std::cout << "ClapTrap 대입 연산자 호출" << std::endl;
-  name = src.name;
-  hitPoints = src.hitPoints;
-  energyPoints = src.energyPoints;
-  attackDamage = src.attackDamage;
+  name = src.getName();
+  hitPoints = src.getHitPoints();
+  energyPoints = src.getEnergyPoints();
+  attackDamage = src.getAttackDamage();
   return *this;
 }
 
-ClapTrap::~ClapTrap(void) {
-  std::cout << "ClapTrap 소멸자 호출" << std::endl;
-}
+ClapTrap::~ClapTrap(void) { std::cout << "ClapTrap 소멸자 호출" << std::endl; }
 
 // Member functions
 
 void ClapTrap::attack(const std::string& target) {
   if (hitPoints == 0 || energyPoints == 0) {
-    std::cout << name << "이(가) 공격할 수 없습니다!" << std::endl;
-    return ;
+    std::cout << "ClapTrap " << name << "이(가) 공격할 수 없습니다!"
+              << std::endl;
+    return;
   }
   --energyPoints;
-  std::cout << name << "이(가) " << target << "을(를) " << attackDamage << "의 데미지로 공격합니다!" << std::endl;
+  std::cout << "ClapTrap " << name << "이(가) " << target << "을(를) "
+            << attackDamage << "의 데미지로 공격합니다!" << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
   if (hitPoints == 0) {
-    std::cout << name << "이(가) 데미지를 입을 수 없습니다!" << std::endl;
-    return ;
+    std::cout << "ClapTrap " << name << "이(가) 이미 파괴되었습니다!"
+              << std::endl;
+    return;
   }
-  std::cout << name << "이(가) " << amount << "의 데미지를 입었습니다!" << std::endl;
-  if (hitPoints < amount)
+  std::cout << "ClapTrap " << name << "이(가) " << amount
+            << "의 데미지를 입었습니다!" << std::endl;
+  if (hitPoints <= amount) {
     hitPoints = 0;
-  else
+    std::cout << "ClapTrap " << name << "이(가) 파괴되었습니다!" << std::endl;
+  } else
     hitPoints -= amount;
 }
 
 void ClapTrap::beRepaired(unsigned int amount) {
-  if (hitPoints == 0 || energyPoints == 0) {
-    std::cout << name << "이(가) 회복할 수 없습니다!" << std::endl;
-    return ;
+  if (hitPoints == 0) {
+    std::cout << "ClapTrap " << name << "이(가) 이미 파괴되었습니다!"
+              << std::endl;
+    return;
+  } else if (energyPoints == 0) {
+    std::cout << "ClapTrap " << name << "이(가) 수리할 수 없습니다!"
+              << std::endl;
+    return;
   }
-  if (hitPoints + amount > 10)
-    hitPoints = 10;
+  if (hitPoints + amount > claptrap::HP)
+    hitPoints = claptrap::HP;
   else
     hitPoints += amount;
   --energyPoints;
-  std::cout << name << "이(가) " << amount << "의 체력을 회복했습니다!" << std::endl;
+  std::cout << "ClapTrap " << name << "이(가) " << amount
+            << "의 체력을 회복합니다!" << std::endl;
 }
 
 // Getters and setters
 
-std::string ClapTrap::getName(void) const {
-  return name;
-}
-unsigned int ClapTrap::getHitPoints(void) const {
-  return hitPoints;
-}
-unsigned int ClapTrap::getAttackDamage(void) const {
-  return attackDamage;
-}
-unsigned int ClapTrap::getEnergyPoints(void) const {
-  return energyPoints;
-}
+std::string ClapTrap::getName(void) const { return name; }
+unsigned int ClapTrap::getHitPoints(void) const { return hitPoints; }
+unsigned int ClapTrap::getAttackDamage(void) const { return attackDamage; }
+unsigned int ClapTrap::getEnergyPoints(void) const { return energyPoints; }
 
-void ClapTrap::setName(std::string name) {
-  this->name = name;
-}
+void ClapTrap::setName(std::string name) { this->name = name; }
 void ClapTrap::setHitPoints(unsigned int hitPoints) {
   this->hitPoints = hitPoints;
 }
@@ -105,4 +111,9 @@ void ClapTrap::setEnergyPoints(unsigned int energyPoints) {
 }
 void ClapTrap::setAttackDamage(unsigned int attackDamage) {
   this->attackDamage = attackDamage;
+}
+
+void ClapTrap::printStatus(void) const {
+  std::cout << getName() << " HP: " << getHitPoints()
+            << " EP: " << getEnergyPoints() << std::endl;
 }
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,48 +6,53 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 13:25:04 by hujeong           #+#    #+#             */
-/*   Updated: 2023/08/26 19:00:27 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/09/02 11:30:59 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
+#include <iostream>
 #include <string>
 
-enum {
-	C_HP = 10,
-	C_EP = 10,
-	C_AD = 0,
+namespace claptrap {
+enum status {
+  HP = 10,
+  EP = 10,
+  AD = 0,
 };
+}
 
 class ClapTrap {
-	public:
-		ClapTrap(void);
-		ClapTrap(std::string name);
-		ClapTrap(const ClapTrap& src);
-		~ClapTrap(void);
-		ClapTrap&		operator=(const ClapTrap& src);
+ public:
+  ClapTrap(void);
+  ClapTrap(std::string name);
+  ClapTrap(const ClapTrap& src);
+  ~ClapTrap(void);
+  ClapTrap& operator=(const ClapTrap& src);
 
-		void			attack(const std::string& target);
-		void			takeDamage(unsigned int amount);
-		void			beRepaired(unsigned int amount);
+  void attack(const std::string& target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
 
-		std::string getName(void) const;
-		unsigned int getHitPoints(void) const;
-		unsigned int getAttackDamage(void) const;
-		unsigned int getEnergyPoints(void) const;
+  std::string getName(void) const;
+  unsigned int getHitPoints(void) const;
+  unsigned int getAttackDamage(void) const;
+  unsigned int getEnergyPoints(void) const;
 
-		void setName(std::string name);
-		void setHitPoints(unsigned int hitPoints);
-		void setEnergyPoints(unsigned int energyPoints);
-		void setAttackDamage(unsigned int attackDamage);
+  void setName(std::string name);
+  void setHitPoints(unsigned int hitPoints);
+  void setEnergyPoints(unsigned int energyPoints);
+  void setAttackDamage(unsigned int attackDamage);
 
-	private:
-		std::string		name;
-		unsigned int	hitPoints;
-		unsigned int	energyPoints;
-		unsigned int	attackDamage;
+  void printStatus(void) const;
+
+ private:
+  std::string name;
+  unsigned int hitPoints;
+  unsigned int energyPoints;
+  unsigned int attackDamage;
 };
 
 #endif
