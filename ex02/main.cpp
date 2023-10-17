5b9bc4bc40b915e250d1d96ea316f6d26832edfb
hujeong <hujeong@student.42seoul.kr>
2023-10-18 03:37:56 +0900

    ex02 끝!

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,18 +6,63 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 15:12:42 by hujeong           #+#    #+#             */
-/*   Updated: 2023/10/15 15:41:08 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/10/18 03:37:22 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
 
-// dynamic_cast 사용해보기
+Base* generate() {
+  srand(time(NULL));
+  int i = rand() % 3;
+  if (i == 0) {
+    std::cout << "A 생성" << std::endl;
+    return new A();
+  } else if (i == 1) {
+    std::cout << "B 생성" << std::endl;
+    return new B();
+  } else {
+    std::cout << "C 생성" << std::endl;
+    return new C();
+  }
+}
 
-Base* generate() {}
-void identify(Base* p) {}
-void identify(Base& p) {}
+void identify(Base* p) {
+  if (dynamic_cast<A*>(p)) {
+    std::cout << "A" << std::endl;
+  } else if (dynamic_cast<B*>(p)) {
+    std::cout << "B" << std::endl;
+  } else if (dynamic_cast<C*>(p)) {
+    std::cout << "C" << std::endl;
+  }
+}
 
-int main() {}
+void identify(Base& p) {
+  try {
+    (void)dynamic_cast<A&>(p);
+    std::cout << "A" << std::endl;
+  } catch (std::exception&) {
+  }
+  try {
+    (void)dynamic_cast<B&>(p);
+    std::cout << "B" << std::endl;
+  } catch (std::exception&) {
+  }
+  try {
+    (void)dynamic_cast<C&>(p);
+    std::cout << "C" << std::endl;
+  } catch (std::exception&) {
+  }
+}
+
+int main() {
+  Base* base = generate();
+  identify(base);
+  identify(*base);
+  delete base;
+  return 0;
+}
