9ba282b28cb67e485ba1c783992b28222964d4f6
hujeong <hujeong@student.42seoul.kr>
2023-10-30 22:33:39 +0900

    ex01 리펙토링

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,20 +6,15 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 07:51:21 by hujeong           #+#    #+#             */
-/*   Updated: 2023/10/21 22:56:56 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/10/30 22:33:39 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T>
-void iter(T *arr, int len, void (*f)(T const &)) {
-  for (int i = 0; i < len; i++) f(arr[i]);
-}
-
-template <typename T>
-void iter(T *arr, int len, void (*f)(T &)) {
+template <typename T, typename func>
+void iter(T *arr, int len, func f) {
   for (int i = 0; i < len; ++i) f(arr[i]);
 }
 
