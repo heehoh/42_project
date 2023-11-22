492ec05b906d5440ceef5e252371a1c14e29ea06
hujeong <hujeong@student.42seoul.kr>
2023-11-22 20:22:07 +0900

    Fix: 헤더 파일 수정 중

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,76 +6,87 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 10:11:19 by hujeong           #+#    #+#             */
-/*   Updated: 2023/11/21 21:22:42 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/11/22 18:56:56 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-#include <fstream>
-#include <iostream>
-#include <sstream>
+// #include <fstream>
+// #include <iostream>
+// #include <sstream>
 
-BitcoinExchange::BitcoinExchange() {}
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &) {}
-BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &) {
-  return *this;
-}
-BitcoinExchange::~BitcoinExchange() {}
+// BitcoinExchange::BitcoinExchange() {}
+// BitcoinExchange::BitcoinExchange(const BitcoinExchange &) {}
+// BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &) {
+//   return *this;
+// }
+// BitcoinExchange::~BitcoinExchange() {}
 
-void checkDate(std::string &date) {
-  if (date.length() != 10)
-    throw std::runtime_error("Error: 날짜 형식이 잘못되었습니다.");
-  if (date[4] != '-' || date[7] != '-')
-    throw std::runtime_error("Error: 날짜 형식이 잘못되었습니다.");
-  for (int i = 0; i < 10; i++) {
-    if (i == 4 || i == 7) continue;
-    if (date[i] < '0' || date[i] > '9')
-      throw std::runtime_error("Error: 날짜 형식이 잘못되었습니다.");
-  }
-}
+// void checkDate(std::string &date) {
+//   if (date.length() != 10)
+//     throw std::runtime_error("Error: 날짜 형식이 잘못되었습니다.");
+//   if (date[4] != '-' || date[7] != '-')
+//     throw std::runtime_error("Error: 날짜 형식이 잘못되었습니다.");
+//   for (int i = 0; i < 10; i++) {
+//     if (i == 4 || i == 7) continue;
+//     if (date[i] < '0' || date[i] > '9')
+//       throw std::runtime_error("Error: 날짜 형식이 잘못되었습니다.");
+//   }
+// }
 
-void checkPrice(std::string &exchangeRate) {
-  int dot = 0;
-  if (exchangeRate[0] == '-')
-    throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
-  for (int i = 0; i < exchangeRate.length(); ++i) {
-    if (exchangeRate[i] == '.') {
-      if (i == 0)
-        throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
-      ++dot;
-      if (dot > 1)
-        throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
-      continue;
-    }
-    if (exchangeRate[i] < '0' || exchangeRate[i] > '9')
-      throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
-  }
-}
+// void checkPrice(std::string &exchangeRate) {
+//   int dot = 0;
+//   if (exchangeRate[0] == '-')
+//     throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
+//   for (int i = 0; i < exchangeRate.length(); ++i) {
+//     if (exchangeRate[i] == '.') {
+//       if (i == 0)
+//         throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
+//       ++dot;
+//       if (dot > 1)
+//         throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
+//       continue;
+//     }
+//     if (exchangeRate[i] < '0' || exchangeRate[i] > '9')
+//       throw std::runtime_error("Error: 가격 형식이 잘못되었습니다.");
+//   }
+// }
 
-void checkFormat(std::string &input) {}
+// void checkFormat(std::string &input) {}
 
-BitcoinExchange::BitcoinExchange(std::string database) {
-  std::ifstream ifs(database);
+BitcoinExchange::BitcoinExchange() {
+  std::ifstream ifs(DATA_BASE);
   std::string line;
-  std::string key;
-  double value;
+  std::stringstream ss;
 
   if (!ifs.is_open())
-    throw std::runtime_error("Error: 데이터 베이스 파일을 열 수 없습니다.");
-  std::getline(ifs, line);
-  while (std::getline(ifs, line)) {
-    checkFormat(line);
-    std::istringstream ss(line);
-    std::getline(ss, key) key = checkDate() exchange_[key] = value;
-  }
+    throw std::runtime_error("에러: 데이터베이스 파일을 열 수 없습니다.");
 }
 
-void BitcoinExchange::printExchange() {
-  std::map<std::string, double>::iterator it = exchange_.begin();
-  while (it != exchange_.end()) {
-    std::cout << it->first << ": " << std::fixed << std::setprecision(2)
-              << it->second << std::endl;
-    it++;
-  }
-}
+// BitcoinExchange::BitcoinExchange(std::string database) {
+//   std::ifstream ifs(database);
+//   std::string line;
+//   std::string key;
+//   double value;
+
+//   if (!ifs.is_open())
+//     throw std::runtime_error("Error: 데이터 베이스 파일을 열 수 없습니다.");
+//   std::getline(ifs, line);
+//   while (std::getline(ifs, line)) {
+//     checkFormat(line);
+//     std::istringstream ss(line);
+//     std::getline(ss, key);
+//     key = checkDate();
+//     exchange_[key] = value;
+//   }
+// }
+
+// void BitcoinExchange::printExchange() {
+//   std::map<std::string, double>::iterator it = exchange_.begin();
+//   while (it != exchange_.end()) {
+//     std::cout << it->first << ": " << std::fixed << std::setprecision(2)
+//               << it->second << std::endl;
+//     it++;
+//   }
+// }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,35 +6,34 @@
 /*   By: hujeong <hujeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 10:11:29 by hujeong           #+#    #+#             */
-/*   Updated: 2023/11/21 21:22:42 by hujeong          ###   ########.fr       */
+/*   Updated: 2023/11/22 18:56:51 by hujeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <fstream>
+#include <iostream>
 #include <map>
-#include <string>
+#include <sstream>
 
+#define DATA_BASE "data.csv"
 class BitcoinExchange {
  private:
-  std::map<std::string, double> exchange_;
-  BitcoinExchange();
+  std::map<std::string, float> exchange_;
+  std::map<std::string, float> value_;
   BitcoinExchange(const BitcoinExchange&);
   BitcoinExchange& operator=(const BitcoinExchange&);
 
-  // 데이터 베이스 파싱
-  // 데이터 베이스 라인 체크
-  // 데이터 베이스 날짜 체크
-  // 데이터 베이스 값 체크(double)
+  void checkDate(std::string date);  // 데이터베이스와 인풋 파일 둘다 검증
+  void checkValue(float value);  // 인풋파일만 검증하면 됨
 
-  // Input 파일 파싱
-  // Input 파일 line 체크
-  // Input 파일 날짜 체크
-  // Input 파일 값체크(int 값)
+  void parseDB();
+  void parseInput();
 
  public:
-  BitcoinExchange(std::string database);
+  BitcoinExchange();
   void printExchange();
   ~BitcoinExchange();
 };
