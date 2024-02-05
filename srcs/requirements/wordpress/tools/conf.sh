34114af4ee3ef66f3ea1bdb55b8b48dcb37ceec7
hujeong <hujeong@student.42seoul.kr>
2024-02-06 03:36:06 +0900

    저장

--- srcs/requirements/wordpress/tools/conf.sh
+++ srcs/requirements/wordpress/tools/conf.sh
@@ -1,12 +1,12 @@
 #https://make.wordpress.org/cli/handbook/how-to/how-to-install/
 
-if [ ! -e "/var/www/wp-config.php" ]; then
-    sudo adduser www-data
-    sudo usermod -a -G www-data www-data
-    wp core download --allow-root
-    wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
-    wp core install --url=$DOMAIN_NAME --title=$WP_NAME --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-    wp user create ${WP_USER} $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
-fi
+# if [ ! -e "/var/www/wp-config.php" ]; then
+#     adduser -u 82 -D -S -G www-data www-data
+#     wp core download --allow-root
+#     wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
+#     wp core install --url=$DOMAIN_NAME --title=$WP_NAME --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+#     wp user create ${WP_USER} $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
+# fi
 
+adduser -u 82 -D -S -G www-data www-data
 /usr/sbin/php-fpm81 -F
